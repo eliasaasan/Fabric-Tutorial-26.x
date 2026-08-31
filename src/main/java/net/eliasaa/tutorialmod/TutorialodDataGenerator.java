3b7d4a97c18c305386d14a53de8cc28f0309595a
Elias Aasan <eliasaasan@gmail.com>
2026-08-31 12:59:40 +0200

    first commit

---
+++ src/main/java/net/eliasaa/tutorialmod/TutorialodDataGenerator.java
@@ -0,0 +1,11 @@
+package net.eliasaa.tutorialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TutorialodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
