1c3744e934c3a436f02b69cb88a128bf1058b117
EliasAa <eliasaasan@gmail.com>
2026-08-31 19:16:16 +0200

    done med fluorite

---
+++ src/main/java/net/eliasaa/tutorialmod/datagen/ModModelProvider.java
@@ -0,0 +1,28 @@
+package net.eliasaa.tutorialmod.datagen;
+
+import net.eliasaa.tutorialmod.item.modItems;
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.minecraft.client.data.models.BlockModelGenerators;
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.model.ModelTemplates;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricPackOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockModelGenerators blockModelGenerators) {
+
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerators itemModelGenerators) {
+
+        itemModelGenerators.generateFlatItem(modItems.FLUORITE, ModelTemplates.FLAT_ITEM);
+
+
+
+    }
+}
